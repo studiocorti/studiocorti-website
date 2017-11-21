6b1bf1418a95b7745e6fcdcdf3bd21eba22145d7
Luca Casartelli <luca@plasticpanda.com>
2017-11-21 14:20:40 +0100

    update header navigation

--- static/js/main.js
+++ static/js/main.js
@@ -5,4 +5,9 @@
   });
   gumshoe.init();
 
+  document.querySelectorAll("#intro img.arrow").on('click', function (el) {
+  	smoothScroll.animateScroll("#servizi");
+  	var section = this.getAttribute("data-section");
+  	window.swiperServizi.slideTo(parseInt(section, 10));
+  })
 })();
